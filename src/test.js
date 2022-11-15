4fb58d4fca1cfe223a5eff3b9dc9ae7f4d527a1e
Fenickss <denis.suxov.1991@mail.ru>
2022-11-15 13:45:40 +0300

    Update test.js

--- src/test.js
+++ src/test.js
@@ -1,4 +1,7 @@
-function square(number) {
-  return number ** 2;
-}
-console.log(square(12));
+function findOdd(a) {}
+findOdd([7], 7);
+findOdd([0], 0);
+findOdd([1, 1, 2], 2);
+findOdd([0, 1, 0, 1, 0], 0);
+findOdd([1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1], 4);
+console.log(findOdd());
